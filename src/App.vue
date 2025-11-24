7b990e2d05b79f0b4a5e00eb16525988dc128578
Leaqmin-raptorgate <m.adityaputra277@gmai.com>
2025-11-24 14:07:01 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-import NavBar from './components/layout/Header.vue'
+import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen bg-[#1e202e]"> 
-    <NavBar />
-    <main class="flex-grow">
+  <div > 
+
+    <Header>
+    </Header>
+
+    <main >
       <RouterView />
     </main>
 
-    <Footer />
+    <Footer>
+    </Footer>
     
   </div>
 </template>
